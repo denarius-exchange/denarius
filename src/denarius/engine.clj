21a0baaa50a5590d9e067f2a122d8ae2fee9de1c
buhos_000 <albrecht.allenstein@gmail.com>
2013-12-25 00:05:23 +0100

    Several changes to the matching engine: - Improved the limit comparison to check the best limit order matching - Improved the matching function to go through all available levels upon exhaustion of the current one. Example: order book {bid: 15 1, 20 1}, incoming ask: 15 2 -> First exhaust the 1 order at 20 and then go for the one at 15.

--- src/denarius/engine.clj
+++ src/denarius/engine.clj
@@ -4,9 +4,9 @@
 (defrecord Book [bid ask])
 
 (defn create-order-book [asset-name]
-   (with-meta 
-     (->Book (ref (sorted-map)) (ref (sorted-map)))
-     {:asset-name asset-name} ))
+  (with-meta 
+    (->Book (ref (sorted-map)) (ref (sorted-map)))
+    {:asset-name asset-name} ))
 
 (defn market-depth
   ([order-book side]
@@ -43,15 +43,37 @@
 
 
 (defn remove-order [^Book book ^Order order]
-    (let [order-id  (:order-id order)
-          side      (:side order)
-          price     (:price order)
-          side-ref  (side book)
-          level-ref (@side-ref price)
-          pred      #(= (:order-id @%) order-id)]
-      (if level-ref
-        (do
-          (dosync (alter level-ref #(remove pred %))) true ))))
+  (let [order-id  (:order-id order)
+        side      (:side order)
+        price     (:price order)
+        side-ref  (side book)
+        level-ref (@side-ref price)
+        pred      #(= (:order-id @%) order-id)]
+    (if level-ref
+      (do
+        (dosync (alter level-ref #(remove pred %))) true ))))
+
+
+(defn best-price [book side]
+  (let [side-ref (if (= side :ask) (:bid book) (:ask book))
+        minmax   (if (= side :ask) max min) 
+        prices   (keys @side-ref)]
+    (if (empty? prices)
+      nil
+      (@side-ref (apply max prices)) )))
+
+
+(defn best-price-limit [book side limit]
+  (let [side-ref (if (= side :ask) (:bid book) (:ask book))
+        minmax   (if (= side :ask) max min)
+        op       (if (= side :ask) <= >=)
+        prices   (keys @side-ref)]
+    (if (empty? prices)
+      nil
+      (let [best (apply minmax prices)]
+        (if (op limit best)
+          (@side-ref best)
+          nil )))))
 
 
 (defn match-order [^Book book ^Order order]
@@ -60,28 +82,28 @@
         price         (:price order)
         order-side    (:side order)
         matching-side (if (= order-side :bid) :ask :bid)
-        side-ref      (matching-side book)
-        level-ref     (@side-ref price)]
-    (if-not level-ref
-      (insert-order book order))
+        side-ref      (matching-side book)]
     (dosync
-      (loop [size (:size order)]
-        (if (empty? @level-ref)
-          (insert-order book (create-order order-id broker-id order-side size price))
-          (let [first-available-ref (last @level-ref)
-                first-available     @(last @level-ref)
-                available-size      (:size first-available)]
-            (if (> available-size size)
-              (let [new-matching (create-order
-                                   (:order-id first-available)
-                                   (:broker-id first-available)
-                                   matching-side
-                                   (- available-size size)
-                                   price)]
-                (dosync (alter first-available-ref (fn [_] new-matching ))))
-              (if (= size available-size)
-                (remove-order book first-available)
-                (do
+      (loop [size      (:size order)
+             level-ref (@side-ref price)]
+        (if-not level-ref
+          (insert-order book order)
+          (if (empty? @level-ref)
+            (insert-order book (create-order order-id broker-id order-side size price))
+            (let [first-available-ref (last @level-ref)
+                  first-available     @(last @level-ref)
+                  available-size      (:size first-available)]
+              (if (> available-size size)
+                (let [new-matching (create-order
+                                     (:order-id first-available)
+                                     (:broker-id first-available)
+                                     matching-side
+                                     (- available-size size)
+                                     price)]
+                  (dosync (ref-set first-available-ref new-matching )))
+                (if (= size available-size)
                   (remove-order book first-available)
-                  (recur (- size available-size)) )))))))))
-  
+                  (do
+                    (remove-order book first-available)
+                    (recur (- size available-size)
+                           (@side-ref (best-price-limit book matching-side price)) )))))))))))
