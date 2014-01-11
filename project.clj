401afdd3710119d79d21886764868ca376e83f7c
buhos_000 <albrecht.allenstein@gmail.com>
2014-01-11 04:44:54 +0100

    Removed HTTP-Kit. The first connector/backend has been fully moved to Aleph (TCP).

--- project.clj
+++ project.clj
@@ -6,7 +6,6 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.logging "0.2.6"]
                  [ring "1.2.1"]
-                 [http-kit "2.1.13"]
                  [compojure "1.1.6"]
                  [org.clojure/data.json "0.2.3"]
                  [aleph "0.3.0"]]
