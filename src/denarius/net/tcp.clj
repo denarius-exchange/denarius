6a6f7bfe5939cf0eb2d2a9ebd0a862bad99ea3e1
analyticbastard <albrecht.allenstein@gmail.com>
2014-04-06 14:10:11 +0200

    Added new list trades protocol command.
    General aesthetics improvements
    
    Issue #25

--- src/denarius/net/tcp.clj
+++ src/denarius/net/tcp.clj
@@ -1,15 +1,16 @@
 (ns denarius.net.tcp)
 
-(def message-request-order 2)
-(def message-request-position 3)
-(def message-request-list 4)
-(def message-request-cancel 5)
+(def message-request-order     2)
+(def message-request-position  3)
+(def message-request-list      4)
+(def message-request-cancel    5)
+(def message-request-trades    6)
 (def message-response-received 0)
-(def message-response-error 1)
+(def message-response-error    1)
 (def message-response-executed 2)
 (def message-response-position 3)
-(def message-response-list 4)
+(def message-response-list     4)
 
 
-(def default-engine-port 7891)
+(def default-engine-port    7891)
 (def default-connector-port 7892)
