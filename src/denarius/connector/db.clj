5d26bd30e344c877b41fde223379b7d2274a85a8
analyticbastard <albrecht.allenstein@gmail.com>
2014-04-10 23:15:29 +0200

    Outstanding orders database functions and protocols created #28

--- src/denarius/connector/db.clj
+++ src/denarius/connector/db.clj
@@ -1,19 +1,19 @@
 (ns denarius.connector.db)
 
 
-(defprotocol db-proto
-  (init-trades-impl [this])
+(defprotocol db-trades
+  (init-trades-impl  [this])
   (insert-trade-impl [this broker-id-1 order-id-1 broker-id-2 order-id-2 size price])
   (query-trades-impl [this broker-id])
-  (stop-trades-impl [this]))
+  (stop-trades-impl  [this]))
 
 
 (defrecord nildb []
-  db-proto
-  (init-trades-impl [this] nil)
+  db-trades
+  (init-trades-impl  [this] nil)
   (insert-trade-impl [this broker-id-1 order-id-1 broker-id-2 order-id-2 size price] nil)
   (query-trades-impl [this broker-id] nil)
-  (stop-trades-impl [this] nil))
+  (stop-trades-impl  [this] nil))
 
 
 ; Database name to be set via configuration
@@ -31,3 +31,32 @@
   (query-trades-impl @dbname broker-id) )
 
 (defn stop-trades [] (stop-trades-impl @dbname))
+
+
+(defprotocol db-orders
+  (init-orders-impl  [this])
+  (insert-order-impl [this order])
+  (query-orders-impl [this broker-id])
+  (alter-size-impl   [this broker-id order-id new-size])
+  (remove-order-impl [this broker-id order-id])
+  (stop-orders-impl  [this]))
+
+
+(defrecord nilorders []
+  db-orders
+  (init-orders-impl  [this] nil)
+  (insert-order-impl [this order] nil)
+  (query-orders-impl [this broker-id] nil)
+  (alter-size-impl   [this broker-id order-id new-size] nil)
+  (remove-order-impl [this broker-id order-id] nil)
+  (stop-orders-impl  [this] nil))
+
+; Order database name to be set via configuration
+(def dborders (atom (nilorders.)))
+
+(defn init-orders  [] (init-orders-impl @dborders))
+(defn insert-order [order] (insert-order-impl @dborders order))
+(defn query-orders [broker-id] (query-orders-impl @dborders broker-id))
+(defn remove-order [broker-id order-id] (remove-order-impl @dborders broker-id order-id))
+(defn alter-size   [broker-id order-id new-size] (alter-size-impl @dborders broker-id order-id new-size))
+(defn stop-orders  [] (stop-orders-impl @dborders))
--- src/denarius/connector/db/mockdb.clj
+++ src/denarius/connector/db/mockdb.clj
@@ -6,8 +6,8 @@
 
 
 (deftype mockdb []
-  db/db-proto
-  (init-trades-impl [this] nil)
+  db/db-trades
+  (init-trades-impl [this] (reset! trades []))
   (insert-trade-impl [this broker-id-1 order-id-1 broker-id-2 order-id-2 size price]
     (let [newtrade      {:broker-id-1 broker-id-1 :order-id-1 order-id-1
                          :broker-id-2 broker-id-2 :order-id-2 order-id-2
@@ -19,3 +19,27 @@
   (query-trades-impl [this broker-id]
     @(@trades broker-id))
   (stop-trades-impl [this] nil) )
+
+
+(def orders (atom []))
+
+(deftype memorders []
+  db-orders
+  (init-orders-impl  [this] (reset! orders {}))
+  (insert-order-impl [this broker-id order]
+    (let [order-id (:order-id order)]
+      (if-let [broker-orders (@orders broker-id)]
+        (swap! broker-orders assoc broker-id (atom {order-id (atom order)}))
+        (swap! orders assoc broker-id (atom {order-id (atom order)}))
+        )))
+  (query-orders-impl [this broker-id] nil)
+  (alter-size-impl   [this broker-id order-id new-size]
+    {:pre (> new-size 0)}
+    (if-let [broker-orders (@orders broker-id)]
+      (let [order (@broker-orders order-id)]
+        (swap! order assoc :size new-size))))
+  (remove-order-impl [this broker-id order-id]
+    (if-let [broker-orders (@orders broker-id)]
+      (swap! broker-orders dissoc order-id)))
+  (stop-orders-impl [this] nil))
+
