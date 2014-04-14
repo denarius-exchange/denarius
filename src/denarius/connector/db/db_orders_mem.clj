6af0a11ee6c802f9a0fddfc6b6b0f5c07363a089
analyticbastard <albrecht.allenstein@gmail.com>
2014-04-15 00:13:05 +0200

    Made functions more rubust to non-existing orders #28

--- src/denarius/connector/db/db_orders_mem.clj
+++ src/denarius/connector/db/db_orders_mem.clj
@@ -10,27 +10,27 @@
     (let [broker-id (:broker-id order)
           order-id (:order-id order)]
       (if-let [broker-orders (@orders broker-id)]
-        (swap! broker-orders assoc broker-id (atom {order-id (atom order)}))
-        (swap! orders assoc broker-id (atom {order-id (atom order)}))
-        )))
-  (query-orders-impl [this broker-id] (@orders broker-id)
-    @(@orders broker-id))
+        (swap! broker-orders assoc order-id (atom order) )
+        (swap! orders assoc broker-id (atom {order-id (atom order)})) )) )
+  (query-orders-impl [this broker-id]
+    (if-let [order-list (@orders broker-id)]
+      @order-list))
   (query-order-impl [this broker-id order-id]
-    (let [broker-oders (@orders broker-id)
-          order        (@broker-oders order-id)]
-      @order))
+    (if-let [broker-oders (@orders broker-id)]
+       (if-let [order     (@broker-oders order-id)]
+         @order)))
   (alter-size-impl   [this broker-id order-id new-size]
     {:pre (>= new-size 0)}
     (if-let [broker-orders (@orders broker-id)]
       (if (= new-size 0)
         (swap! broker-orders dissoc order-id)
-        (let [order (@broker-orders order-id)]
+        (if-let [order (@broker-orders order-id)]
           (swap! order assoc :size new-size)))))
   (decrease-size-impl [this broker-id order-id amount]
-    (let [order        (db/query-order-impl this broker-id order-id)
-          current-size (:size order)
-          new-size     (- current-size amount)]
-      (db/alter-size-impl this broker-id order-id new-size)))
+    (if-let [order       (db/query-order-impl this broker-id order-id)]
+      (let [current-size (:size order)
+            new-size     (- current-size amount)]
+        (db/alter-size-impl this broker-id order-id new-size))))
   (remove-order-impl [this broker-id order-id]
     (if-let [broker-orders (@orders broker-id)]
       (swap! broker-orders dissoc order-id)))
