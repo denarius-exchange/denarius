4c2583cc222aa439ed3afbe4925a82f5f99eb5c4
analyticbastard <albrecht.allenstein@gmail.com>
2014-04-14 23:15:21 +0200

    Improved order manipulation in the connector component. Added new functionality such as atomic decrease order size and automatic order removal if size reaches zero #28

--- src/denarius/connector/db/db_orders_mem.clj
+++ src/denarius/connector/db/db_orders_mem.clj
@@ -1,18 +1,23 @@
 (ns denarius.connector.db.db-orders-mem
   (:require [denarius.connector.db-orders :as db]))
 
-(def orders (atom []))
+(def orders (atom {}))
 
 (deftype db-orders-mem []
   db/db-orders
   (init-orders-impl  [this] (reset! orders {}))
-  (insert-order-impl [this broker-id order]
-    (let [order-id (:order-id order)]
+  (insert-order-impl [this order]
+    (let [broker-id (:broker-id order)
+          order-id (:order-id order)]
       (if-let [broker-orders (@orders broker-id)]
         (swap! broker-orders assoc broker-id (atom {order-id (atom order)}))
         (swap! orders assoc broker-id (atom {order-id (atom order)}))
         )))
-  (query-orders-impl [this broker-id] nil)
+  (query-orders-impl [this broker-id] (@orders broker-id))
+  (query-order-impl [this broker-id order-id]
+    (let [broker-oders (@orders broker-id)
+          order        (@broker-oders order-id)]
+      @order))
   (alter-size-impl   [this broker-id order-id new-size]
     {:pre (>= new-size 0)}
     (if-let [broker-orders (@orders broker-id)]
@@ -20,6 +25,11 @@
         (swap! broker-orders dissoc order-id)
         (let [order (@broker-orders order-id)]
           (swap! order assoc :size new-size)))))
+  (decrease-size-impl [this broker-id order-id amount]
+    (let [order        (db/query-order-impl this broker-id order-id)
+          current-size (:size order)
+          new-size     (- current-size amount)]
+      (db/alter-size-impl this broker-id order-id new-size)))
   (remove-order-impl [this broker-id order-id]
     (if-let [broker-orders (@orders broker-id)]
       (swap! broker-orders dissoc order-id)))
--- src/denarius/connector/db_orders.clj
+++ src/denarius/connector/db_orders.clj
@@ -1,22 +1,26 @@
 (ns denarius.connector.db-orders)
 
 (defprotocol db-orders
-  (init-orders-impl  [this])
-  (insert-order-impl [this order])
-  (query-orders-impl [this broker-id])
-  (alter-size-impl   [this broker-id order-id new-size])
-  (remove-order-impl [this broker-id order-id])
-  (stop-orders-impl  [this]))
+  (init-orders-impl   [this])
+  (insert-order-impl  [this order])
+  (query-orders-impl  [this broker-id])
+  (query-order-impl   [this broker-id order-id])
+  (alter-size-impl    [this broker-id order-id new-size])
+  (decrease-size-impl [this broker-id order-id amount])
+  (remove-order-impl  [this broker-id order-id])
+  (stop-orders-impl   [this]))
 
 
 (defrecord nilorders []
   db-orders
-  (init-orders-impl  [this] nil)
-  (insert-order-impl [this order] nil)
-  (query-orders-impl [this broker-id] nil)
-  (alter-size-impl   [this broker-id order-id new-size] nil)
-  (remove-order-impl [this broker-id order-id] nil)
-  (stop-orders-impl  [this] nil))
+  (init-orders-impl   [this] nil)
+  (insert-order-impl  [this order] nil)
+  (query-orders-impl  [this broker-id] nil)
+  (query-order-impl   [this broker-id order-id] nil)
+  (alter-size-impl    [this broker-id order-id new-size] nil)
+  (decrease-size-impl [this broker-id order-id amount] nil)
+  (remove-order-impl  [this broker-id order-id] nil)
+  (stop-orders-impl   [this] nil))
 
 ; Order database name to be set via configuration
 (def dborders (atom (nilorders.)))
@@ -24,6 +28,8 @@
 (defn init-orders  [] (init-orders-impl @dborders))
 (defn insert-order [order] (insert-order-impl @dborders order))
 (defn query-orders [broker-id] (query-orders-impl @dborders broker-id))
+(defn query-order  [broker-id order-id] (query-order-impl @dborders broker-id order-id))
 (defn remove-order [broker-id order-id] (remove-order-impl @dborders broker-id order-id))
 (defn alter-size   [broker-id order-id new-size] (alter-size-impl @dborders broker-id order-id new-size))
+(defn decrease-size [broker-id order-id amount] (decrease-size-impl @dborders broker-id order-id amount))
 (defn stop-orders  [] (stop-orders-impl @dborders))
