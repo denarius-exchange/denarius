f0c1fe2057d251d366ce5a94c73a741edc45c11d
analyticbastard <albrecht.allenstein@gmail.com>
2014-04-18 11:09:37 +0200

    Fixed bugs in the implementation of the deskinfo-file and general deskinfo interface #30

--- src/denarius/connector/db/db_deskinfo_file.clj
+++ src/denarius/connector/db/db_deskinfo_file.clj
@@ -4,23 +4,26 @@
 
 (def deskinfo (atom {}))
 
-(deftype db-trades-file [dbopt]
-  db/db-trades
+(deftype db-deskinfo-file [dbopt]
+  db/db-deskinfo
   (init-deskinfo-impl [this]
-    (let [info (slurp "resources/db_deskinfo_file.json")]
-      (reset! deskinfo (json/read-str info))
-      (println @deskinfo)))
+    (let [json-info (slurp "resources/db_deskinfo_file.json")
+          json-info (json/read-str json-info)
+          map-info  (into {} (for [[k v] json-info]
+                               [(Long/parseLong k) (into {} (map (fn [ki vi] (try [(keyword ki) (Long/parseLong vi)]
+                                                                                  (catch Exception e [(keyword ki) vi]))) (keys v) (vals v)))]))]
+      (reset! deskinfo map-info)))
   (query-deskinfo-available-funds-impl [this broker-id]
-    (if-let [broker-info (@deskinfo broker-id)]
-      (let [amount (:amount @broker-info)
-            margin (:margin @broker-info)]
+    (if-let [broker-info (@deskinfo broker-id )]
+      (let [amount (:amount broker-info)
+            margin (:margin broker-info)]
         (- amount margin))
       nil))
-  (query-deskinfo-cantrade [this broker-id initial]
-    (if-let [available (query-deskinfo-available-funds-impl)]
+  (query-deskinfo-cantrade-impl [this broker-id initial]
+    (if-let [available (db/query-deskinfo-available-funds broker-id)]
       (> available 0)
       false))
-  (stop-trades-impl [this] nil) )
+  (stop-deskinfo-impl [this] nil) )
 
 
 
