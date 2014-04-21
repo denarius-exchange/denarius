07981075c987da45f7b03bad074bf9c4439575fa
analyticbastard <albrecht.allenstein@gmail.com>
2014-04-21 10:23:38 +0200

    Configuration file version for MySQL

--- resources/config.clj
+++ resources/config.clj
@@ -1,7 +1,7 @@
 {
  :engine {}
- :connector {:database-trades {:class "denarius.connector.db.db_trades_mem.db-trades-mem"
-                               :options nil}
+ :connector {:database-trades {:class "denarius.connector.db.db_trades_mysql.db-trades-mysql"
+                               :options {:host "192.168.0.39" :port "3306" :dbname "denarius" :user "root" :passw ""}}
              :database-orders {:class "denarius.connector.db.db_orders_mem.db-orders-mem"
                                :options nil}
              :database-deskinfo {:class "denarius.connector.db.db_deskinfo_file.db-deskinfo-file"
